21762f3455aabcd8a5a656b9f96f25b2930a6655
Bao Nguyen <baonguyen@Baos-MacBook-Air.local>
2023-08-30 21:11:49 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+	import Counter from './Counter.svelte';
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>
+		Red Acres Farm
+	</h1>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+		color: rgba(125, 0, 0, 0.9);
+		font-family: Georgia, 'Times New Roman', Times, serif;
+		font-size: 10em;
+		-webkit-text-stroke: 5px white; /* width and color */
+	}
+</style>
